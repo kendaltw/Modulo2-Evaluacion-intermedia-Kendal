b3c39022444416ca79007a7626ba52382639e7c4
kendaltw <kendaltellez@gmail.com>
2024-10-25 10:41:53 +0200

    conditional

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 "use strict";
 
+const numberInput = document.querySelector(".js-number-input");
+const submitButton = document.querySelector(".js-submit");
+const hint = document.querySelector(".js-hint");
+const chances = document.querySelector(".js-chances");
 
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+
+
+function handleClick(ev) {
+    ev.preventDefault();
+    const input = numberInput.value;
+
+    if (input == randomNumber) {
+        hint.innerHTML = "¡Has ganado!";
+    } else if (input < randomNumber) {
+        hint.innerHTML = "Demasiado bajo";
+    } else if (input > randomNumber) {
+        hint.innerHTML = "Demasiado alto";
+    }
+
+
+}
+
+
+
+
+submitButton.addEventListener("click", handleClick);
